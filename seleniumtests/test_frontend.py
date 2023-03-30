66d4344113015420556d0c02c956a5168ee457ae
divc13 <divyanshc21@iitk.ac.in>
2023-03-30 23:54:55 +0530

    login templates changed

--- seleniumtests/test_frontend.py
+++ seleniumtests/test_frontend.py
@@ -14,9 +14,28 @@ class tests:
         self.driver.find_element(By.CLASS_NAME, "e1_226 button2").click()
         
     def login_student_1(self):
+        self.driver.find_element(By.ID, "username").send_keys("student1")
+        self.driver.find_element(By.ID, "password").send_keys("1234@Div")
+        self.driver.find_element(By.CLASS_NAME, "submit_btn").click()
         
+    def test1(self):
+        
+        self.driver.get("http://127.0.0.1:8000/")
+        
+        # navigation of all sections and subsections
+        self.login_student_1()
+        
+        # clicking on mess
+        self.driver.find_elements(By.CLASS_NAME, "e1_247")[0].click()
+        
+        # subsections of mess
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_9")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_4")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_8")[0].click()
         
 test = tests()
 test.setup_method()
-test.test()
+test.test1()
 test.quit()
