caa32bfb3091306137d36f94def64841d7cd68a4
divc13 <divyanshc21@iitk.ac.in>
2023-03-31 02:19:46 +0530

    changes in template

--- seleniumtests/test_frontend.py
+++ seleniumtests/test_frontend.py
@@ -11,22 +11,42 @@ class tests:
         self.driver.quit()
 
     def logout(self):
-        self.driver.find_element(By.CLASS_NAME, "e1_226 button2").click()
+        self.driver.find_element(By.CLASS_NAME, "e1_226").click()
         
     def login_student_1(self):
-        self.driver.find_element(By.ID, "username").send_keys("student1")
+        self.driver.find_element(By.ID, "username").send_keys("s1")
+        self.driver.find_element(By.ID, "password").send_keys("1234@Div")
+        self.driver.find_element(By.CLASS_NAME, "submit_btn").click()
+        
+    def login_mess_manager(self):
+        self.driver.find_element(By.ID, "username").send_keys("mm")
+        self.driver.find_element(By.ID, "password").send_keys("1234@Div")
+        self.driver.find_element(By.CLASS_NAME, "submit_btn").click()
+        
+    def login_canteen_manager(self):
+        self.driver.find_element(By.ID, "username").send_keys("cm")
+        self.driver.find_element(By.ID, "password").send_keys("1234@Div")
+        self.driver.find_element(By.CLASS_NAME, "submit_btn").click()
+        
+    def login_hall_manager(self):
+        self.driver.find_element(By.ID, "username").send_keys("hm")
+        self.driver.find_element(By.ID, "password").send_keys("1234@Div")
+        self.driver.find_element(By.CLASS_NAME, "submit_btn").click()
+        
+    def login_sports_secy(self):
+        self.driver.find_element(By.ID, "username").send_keys("ss")
         self.driver.find_element(By.ID, "password").send_keys("1234@Div")
         self.driver.find_element(By.CLASS_NAME, "submit_btn").click()
         
     def test1(self):
+        # navigation of all sections and subsections of student side
         
-        self.driver.get("http://127.0.0.1:8000/")
+        self.driver.get("https://upha.pythonanywhere.com/")
         
-        # navigation of all sections and subsections
         self.login_student_1()
         
         # clicking on mess
-        self.driver.find_elements(By.CLASS_NAME, "e1_247")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e1_246")[0].click()
         
         # subsections of mess
         self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
@@ -35,7 +55,253 @@ class tests:
         self.driver.find_elements(By.CLASS_NAME, "e2_4")[0].click()
         self.driver.find_elements(By.CLASS_NAME, "e2_8")[0].click()
         
+        # clicking on canteen
+        self.driver.find_elements(By.CLASS_NAME, "e1_242")[0].click()
+        
+        # subsections of canteen
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_9")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_4")[0].click()
+        
+        # clicking on booking
+        self.driver.find_elements(By.CLASS_NAME, "e1_234")[0].click()
+        
+        # subsections of booking
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_9")[0].click()
+        
+        # clicking on cleaning
+        self.driver.find_elements(By.CLASS_NAME, "e1_238")[0].click()
+        
+        # subsections of cleaning
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_9")[0].click()
+        
+        # clicking on my account
+        self.driver.find_elements(By.CLASS_NAME, "e1_230")[0].click()
+        
+        # subsections of my account
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        
+        # clicking on home through title
+        self.driver.find_elements(By.CLASS_NAME, "e1_261")[0].click()
+        
+        # clicking on canteen
+        self.driver.find_elements(By.CLASS_NAME, "e1_242")[0].click()
+        
+        # clicking on home
+        self.driver.find_elements(By.CLASS_NAME, "e1_250")[0].click()
+        
+        # logging out
+        self.logout()
+        
+        
+    def test2(self):
+        # navigation of all sections and subsections of Mess Manager
+        self.driver.get("https://upha.pythonanywhere.com/")
+        
+        self.login_mess_manager()
+        
+        # clicking on mess
+        self.driver.find_elements(By.CLASS_NAME, "e1_246")[0].click()
+        
+        # subsections of mess
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_4")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_31")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_32")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_33")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_34")[0].click()
+        
+        # clicking on home
+        self.driver.find_elements(By.CLASS_NAME, "e1_250")[0].click()
+        
+        # logging out
+        self.logout()
+        
+    def test3(self):
+        # navigation of all sections and subsections of Canteen Manager
+        self.driver.get("https://upha.pythonanywhere.com/")
+        
+        self.login_canteen_manager()
+        
+        # clicking on canteen
+        self.driver.find_elements(By.CLASS_NAME, "e1_246")[0].click()
+        
+        # subsections of canteen
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_4")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_31")[0].click()
+        
+        # clicking on home
+        self.driver.find_elements(By.CLASS_NAME, "e1_250")[0].click()
+        
+        # logging out
+        self.logout()
+        
+    def test4(self):
+        # navigation of all sections and subsections of Hall Manager
+        self.driver.get("https://upha.pythonanywhere.com/")
+        
+        self.login_hall_manager()
+        
+        # clicking on hall
+        self.driver.find_elements(By.CLASS_NAME, "e1_246")[0].click()
+        
+        # subsections of hall
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        
+        # clicking on home
+        self.driver.find_elements(By.CLASS_NAME, "e1_250")[0].click()
+        
+        # logging out
+        self.logout()
+        
+    def test5(self):
+        # navigation of all sections and subsections of Sports secy
+        self.driver.get("https://upha.pythonanywhere.com/")
+        
+        self.login_sports_secy()
+        
+        # clicking on sports
+        self.driver.find_elements(By.CLASS_NAME, "e1_246")[0].click()
+        
+        # subsections of sports
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "e2_5")[0].click()
+        
+        # clicking on home
+        self.driver.find_elements(By.CLASS_NAME, "e1_250")[0].click()
+        
+        # logging out
+        self.logout()
+        
+    def test6(self):
+        # mess manager modify menu
+        self.driver.get("https://upha.pythonanywhere.com/")
+        
+        self.login_mess_manager()
+        
+        # clicking on mess
+        self.driver.find_elements(By.CLASS_NAME, "e1_246")[0].click()
+        
+        
+        # subsections of mess
+        self.driver.find_elements(By.CLASS_NAME, "e2_3")[0].click()
+        
+        
+        # adding item
+        self.driver.find_elements(By.NAME, "add_hidden_item")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[0].click()
+        self.driver.find_elements(By.NAME, "a4")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[0].click()
+        self.driver.find_elements(By.NAME, "b1")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[0].send_keys("Rajma Chawal")
+        
+        # adding item
+        self.driver.find_elements(By.NAME, "add_hidden_item")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[1].click()
+        self.driver.find_elements(By.NAME, "a2")[1].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[1].click()
+        self.driver.find_elements(By.NAME, "b2")[1].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[1].send_keys("Pav Bhaji")
+        
+        # submitting
+        self.driver.find_elements(By.NAME, "submit")[0].click()
+
+        
+        # editting
+        self.driver.find_elements(By.NAME, "edit")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[1].click()
+        self.driver.find_elements(By.NAME, "a0")[1].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[1].click()
+        self.driver.find_elements(By.NAME, "b0")[1].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[1].clear()
+        self.driver.find_elements(By.CLASS_NAME, "c")[1].send_keys("Sandwitch")
+        
+        # submitting
+        self.driver.find_elements(By.NAME, "submit")[0].click()
+        
+        # editting
+        self.driver.find_elements(By.NAME, "edit")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[0].click()
+        self.driver.find_elements(By.NAME, "a1")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[0].click()
+        self.driver.find_elements(By.NAME, "b2")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[0].clear()
+        self.driver.find_elements(By.CLASS_NAME, "c")[0].send_keys("Masala Dosa")
+        
+        # adding item
+        self.driver.find_elements(By.NAME, "add_hidden_item")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[2].click()
+        self.driver.find_elements(By.NAME, "a2")[2].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[2].click()
+        self.driver.find_elements(By.NAME, "b2")[2].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[2].send_keys("Pav Bhaji")
+        
+        # submitting
+        self.driver.find_elements(By.NAME, "submit")[0].click()
+        
+        # deleting
+        self.driver.find_elements(By.NAME, "delete")[1].click()
+        
+        # adding item
+        self.driver.find_elements(By.NAME, "add_hidden_item")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[2].click()
+        self.driver.find_elements(By.NAME, "a3")[2].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[2].click()
+        self.driver.find_elements(By.NAME, "b0")[2].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[2].send_keys("Idli Sambhar")
+        
+        # deleting
+        self.driver.find_elements(By.NAME, "delete")[2].click()
+        
+        # adding item
+        self.driver.find_elements(By.NAME, "add_hidden_item")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[2].click()
+        self.driver.find_elements(By.NAME, "a3")[2].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[2].click()
+        self.driver.find_elements(By.NAME, "b0")[2].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[2].send_keys("Idli Sambhar")
+        
+        # deleting
+        self.driver.find_elements(By.NAME, "delete")[1].click()
+        
+        # editting
+        self.driver.find_elements(By.NAME, "edit")[0].click()
+        self.driver.find_elements(By.CLASS_NAME, "a")[1].click()
+        self.driver.find_elements(By.NAME, "a1")[1].click()
+        self.driver.find_elements(By.CLASS_NAME, "b")[1].click()
+        self.driver.find_elements(By.NAME, "b2")[1].click()
+        self.driver.find_elements(By.CLASS_NAME, "c")[1].clear()
+        self.driver.find_elements(By.CLASS_NAME, "c")[1].send_keys("Masala Dosa")
+        
+        # deleting
+        self.driver.find_elements(By.NAME, "delete")[1].click()
+        
+        # deleting
+        self.driver.find_elements(By.NAME, "delete")[0].click()       
+       
+        
+        
+        
+    def main_test(self):
+        self.test1()
+        self.test2()        
+        self.test3()
+        self.test4()
+        self.test5()
+        self.test6()
+
+        
 test = tests()
 test.setup_method()
-test.test1()
+test.main_test()
 test.quit()
