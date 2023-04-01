c890ec4bbf232dbc46852225457e9d25c4d55f18
divc13 <divyanshc21@iitk.ac.in>
2023-04-01 15:03:09 +0530

    tests completed - selenium

--- Home/views.py
+++ Home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def Make_Homepage (request):
+    # rendering the home page
     if request.user.is_authenticated:
         return render (request, 'Home.html')
     else:
