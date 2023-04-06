6b2a89bdc0146cf8c573ba46626c08a68398a98b
Soham <soham.bharambe@gmail.com>
2023-04-06 12:09:46 +0530

    change credential

--- UPHA/settings.py
+++ UPHA/settings.py
@@ -150,6 +150,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'unifiedportalforhallautomation@gmail.com'
-EMAIL_HOST_PASSWORD = 'ocikrvfooiwsanzv'
+EMAIL_HOST_PASSWORD = 'xfcamltmurzurwey'
 
 AUTH_USER_MODEL = 'Login.User_class'
